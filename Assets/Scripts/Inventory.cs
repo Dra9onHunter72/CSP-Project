c2cdd4f7f099df280e214d1e75f439769a03ed72
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2018-03-06 13:52:17 -0700

    I finished the script for Inventory and started another script for InventoryEditor

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class Inventory : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+public class Inventory : MonoBehaviour 
+{
+	public Image[] itemImages = new Image[numItemSlots];
+	public Item[] items = new Item[numItemSlots];
+	public const int numItemSlots = 4;
+	public void AddItem(ContextMenuItemAttribute itemToAdd)
+	{
+		for (int i = 0; i < items.Length; i++) 
+		{
+			if (items [i] == null) 
+			{
+				items [i] = itemToAdd;
+				itemImages [i].sprite = itemToAdd.sprite;
+				itemImages [i].enabled = true;
+				return;
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void RemoveItem (Item itemToRemove)
+	{
+		for (int i = 0; i < items.Length; i++) 
+		{
+			if (items [i] == itemToRemove) 
+			{
+				items [i] = null;
+				itemImages [i].sprite = null;
+				itemImages [i].enabled = false;
+				return;
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/InventoryEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+[CustomEditor(typeof(Inventory))]
+
+public class InventoryEditor : Editor
+{
+	private bool[] showItemSlots = new bool[Inventory.numItemSlots];
+	private SerializedProperty itemImagesProperty;
+	private SerializedProperty itemsProperty;
+	private const string inventoryPropItemsImagesName = "itemImages";
+	private const string inventoryPropItemsName = "items";
+
+	private void OnEnable ()
+	{
+		itemImagesProperty = serializedObject.FindProperty (inventoryPropItemsImagesName);
+		itemsProperty = serializedObject.FindProperty (inventoryPropItemsName);
+	}
+
+	public override void OnInspectorGUI ()
+	{
+		serializedObject.Update ();
+		for (int i = 0; i < Inventory.numItemSlots; i++) 
+		{
+			ItemSlotGUI (i);
+		}
+		serializedObject.ApplyModifiedProperties ();
+	}
+
+	private void ItemSlotGUI (int index)
+	{
+		EditorGUILayout.BeginVertical (GUI.skin.box);
+		EditorGUI.indentLevel++;
+	}
+
+}
